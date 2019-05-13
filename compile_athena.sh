c962cdaab8bce58f169a069c2fac4ddc3c1f8728
ku69yet <ku69yet@login01.ara>
2019-05-13 11:44:19 +0200

    Added script to compile athena for the wave equation with different options

--- compile_athena.sh
+++ compile_athena.sh
@@ -1,9 +1,21 @@
 if [ -z "$1" ]; then
-    echo "Usage: $0 name_athena_exe"
+    echo "Usage: $0 dimensions[1|2|3] nghost[1|2|3] parallelization[mpi|omp](optional)"
+    echo "E.g. 'bash $0 3 2 mpi omp' will generate bin/athena_wave_3d_2g_mpi_omp"
     exit 1
 fi
 
+STR="--prob=wave_test_$1d -w --nghost=$2"
+NEWNAME="athena_wave_"$1"d_"$2"g"
+if [ -n "$3" ]; then
+    STR=$STR" -$3"
+    NEWNAME=$NEWNAME"_$3"
+fi
+if [ -n "$4" ]; then
+    STR=$STR" -$4"
+    NEWNAME=$NEWNAME"_$4"
+fi
+
 make clean
-python configure.py --prob=wave_test -w --nghost=2
+python configure.py $STR 
 make -j4
-mv bin/athena bin/$1
+mv bin/athena bin/$NEWNAME
