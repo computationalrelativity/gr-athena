392e17a3e2ea5df14fd175dd1fbf816235d31a91
William Cook <william.cook@uni-jena.de>
2022-09-22 16:23:38 +0200

    gr_rns_tov problem added, calculates and reads in initial data from linked RNS library from rnsc library

---
+++ src/hydro/initial_data/rns_id.cpp
@@ -0,0 +1,336 @@
+//========================================================================================
+// Athena++ astrophysical MHD code
+// Copyright(C) 2014 James M. Stone <jmstone@princeton.edu> and other code contributors
+// Licensed under the 3-clause BSD License, see LICENSE file for details
+//========================================================================================
+//! \file puncture_z4c.cpp
+//  \brief implementation of functions in the Z4c class for initializing puntures evolution
+
+// C++ standard headers
+#include <cmath> // pow
+
+// Athena++ headers
+#include "../../z4c/z4c.hpp"
+#include "../../z4c/z4c_macro.hpp"
+#include "../../coordinates/coordinates.hpp"
+#include "../../mesh/mesh.hpp"
+#include "../hydro.hpp"
+
+
+#ifdef RNS
+#include "RNS.h"
+#endif
+//----------------------------------------------------------------------------------------
+// \!fn void Z4c::ADMTwoPunctures(AthenaArray<Real> & u)
+// \brief Initialize ADM vars to two punctures
+
+void Hydro::RNS_Metric(ParameterInput *pin, AthenaArray<Real> & u_adm, AthenaArray<Real> & u, AthenaArray<Real> & u1, ini_data *data)
+{
+  bool verbose = pin->GetOrAddBoolean("problem", "verbose", 0);
+  
+  //if(verbose)
+  //  Z4c::DebugInfoVars();
+
+
+  MeshBlock * pmb = pmy_block;
+  Coordinates * pco = pmb->pcoord;
+  Z4c * pz4c = pmb->pz4c;
+
+  Z4c::ADM_vars adm;
+  pz4c->SetADMAliases(u_adm, adm);
+  Z4c::Z4c_vars z4c;
+  pz4c->SetZ4cAliases(u, z4c);
+  Z4c::Z4c_vars z4c_1;
+  pz4c->SetZ4cAliases(u1, z4c_1);
+
+  // Flat spacetime
+//  ADMMinkowski(u_adm);
+
+  //--
+
+  // construct initial data set
+  if(verbose)
+    std::cout << "Generating rns data." << std::endl;
+
+  if(verbose)
+    std::cout << "Done!" << std::endl;
+
+  // interpolate to ADM variables based on solution
+  if(verbose)
+    std::cout << "Interpolating current MeshBlock." << std::endl;
+
+  int imin[3] = {0, 0, 0};
+
+  // dimensions of block in each direction
+  //int n[3] = {(*pmb).block_size.nx1 + 2 * GSIZEI,
+  //            (*pmb).block_size.nx2 + 2 * GSIZEJ,
+  //            (*pmb).block_size.nx3 + 2 * GSIZEK};
+  int n[3] = {pz4c->mbi.nn1, pz4c->mbi.nn2, pz4c->mbi.nn3};
+  int sz = n[0] * n[1] * n[2];
+  // this could be done instead by accessing and casting the Athena vars but
+  // then it is coupled to implementation details etc.
+  Real *gxx = new Real[sz], *gyy = new Real[sz], *gzz = new Real[sz];
+  Real *gxy = new Real[sz], *gxz = new Real[sz], *gyz = new Real[sz];
+
+  Real *Kxx = new Real[sz], *Kyy = new Real[sz], *Kzz = new Real[sz];
+  Real *Kxy = new Real[sz], *Kxz = new Real[sz], *Kyz = new Real[sz];
+  Real *psi = new Real[sz];
+  Real *alp = new Real[sz];
+  Real *betax = new Real[sz], *betay = new Real[sz],*betaz = new Real[sz];
+
+  Real *x = new Real[n[0]];
+  Real *y = new Real[n[1]];
+  Real *z = new Real[n[2]];
+
+  // need to populate coordinates
+  for(int ix_I = 0; ix_I < n[0]; ix_I++){
+    //x[ix_I] = pco->x1v(ix_I);
+    x[ix_I] = pz4c->mbi.x1(ix_I);
+  }
+
+  for(int ix_J = 0; ix_J < n[1]; ix_J++){
+    //y[ix_J] = pco->x2v(ix_J);
+    y[ix_J] = pz4c->mbi.x2(ix_J);
+  }
+
+  for(int ix_K = 0; ix_K < n[2]; ix_K++){
+    //z[ix_K] = pco->x3v(ix_K);
+    z[ix_K] = pz4c->mbi.x3(ix_K);
+  }
+
+  RNS_Cartesian_interpolation
+    (data, // struct containing the previously calculated solution
+     imin, // min, max idxs of Cartesian Grid in the three directions
+     n,    // 
+     n,    // total number of indices in each direction
+     x,    // x,         // Cartesian coordinates
+     y,    // y,
+     z,    // z,
+     alp,  // alp,       // lapse
+     betax,  // betax,   // shift vector
+     betay, // betay,
+     betaz, // betaz,
+     gxx,  // gxx,       // metric components
+     gxy,  // gxy,
+     gxz,  // gxz,
+     gyy,  // gyy,
+     gyz,  // gyz,
+     gzz,  // gzz,
+     Kxx,  // kxx,       // extrinsic curvature components
+     Kxy,  // kxy,
+     Kxz,  // kxz,
+     Kyy,  // kyy,
+     Kyz,  // kyz,
+     Kzz,  // kzz
+     NULL, // rho
+     NULL, // epsl
+     NULL, // vx
+     NULL, // vy
+     NULL, // vz
+     NULL, // ux
+     NULL, // uy
+     NULL, // uz
+     NULL  // pres
+);
+
+
+  int flat_ix;
+
+  GLOOP3(k,j,i){
+    flat_ix = i + n[0]*(j + n[1]*k);
+
+    adm.g_dd(0, 0, k, j, i) = gxx[flat_ix];
+    adm.g_dd(1, 1, k, j, i) = gyy[flat_ix];
+    adm.g_dd(2, 2, k, j, i) = gzz[flat_ix];
+    adm.g_dd(0, 1, k, j, i) = gxy[flat_ix];
+    adm.g_dd(0, 2, k, j, i) = gxz[flat_ix];
+    adm.g_dd(1, 2, k, j, i) = gyz[flat_ix];
+
+    adm.K_dd(0, 0, k, j, i) = Kxx[flat_ix];
+    adm.K_dd(1, 1, k, j, i) = Kyy[flat_ix];
+    adm.K_dd(2, 2, k, j, i) = Kzz[flat_ix];
+    adm.K_dd(0, 1, k, j, i) = Kxy[flat_ix];
+    adm.K_dd(0, 2, k, j, i) = Kxz[flat_ix];
+    adm.K_dd(1, 2, k, j, i) = Kyz[flat_ix];
+
+    z4c_1.alpha(k,j,i) = z4c.alpha(k,j,i)        = alp[flat_ix];
+    z4c_1.beta_u(0,k,j,i) = z4c.beta_u(0,k,j,i)     = betax[flat_ix];
+    z4c_1.beta_u(1,k,j,i) = z4c.beta_u(1,k,j,i)     = betay[flat_ix];
+    z4c_1.beta_u(2,k,j,i) = z4c.beta_u(2,k,j,i)     = betaz[flat_ix];
+  }
+
+
+  free(gxx); free(gyy); free(gzz);
+  free(gxy); free(gxz); free(gyz);
+
+  free(Kxx); free(Kyy); free(Kzz);
+  free(Kxy); free(Kxz); free(Kyz);
+
+  free(psi); free(alp);
+  free(betax); free(betay); free(betaz);
+
+  free(x); free(y); free(z);
+
+  if(verbose)
+    std::cout << "\n\n<-Z4c::ADMTwoPunctures\n\n";
+}
+
+
+
+
+
+void Hydro::RNS_Hydro(ParameterInput *pin, AthenaArray<Real> & w, AthenaArray<Real> & w1, AthenaArray<Real> & w_init, ini_data *data)
+{
+  bool verbose = pin->GetOrAddBoolean("problem", "verbose", 0);
+  
+  //if(verbose)
+  //  Z4c::DebugInfoVars();
+
+  Real rhoc = pin->GetReal("problem","rhoc");
+  Real k_adi = pin->GetReal("hydro","k_adi");
+  Real gamma_adi = pin->GetReal("hydro","gamma");
+  Real fatm = pin->GetReal("problem","fatm");
+  Real pres_pert = pin->GetReal("problem","pres_pert");
+  Real v_pert = pin->GetReal("problem","v_pert");
+
+  MeshBlock * pmb = pmy_block;
+  Coordinates * pco = pmb->pcoord;
+  Z4c * pz4c = pmb->pz4c;
+
+  // Flat spacetime
+//  ADMMinkowski(u_adm);
+
+  //--
+
+  // construct initial data set
+  if(verbose)
+    std::cout << "Generating two puncture data." << std::endl;
+
+  if(verbose)
+    std::cout << "Done!" << std::endl;
+
+  // interpolate to ADM variables based on solution
+  if(verbose)
+    std::cout << "Interpolating current MeshBlock." << std::endl;
+
+  int imin[3] = {0, 0, 0};
+
+  // dimensions of block in each direction
+  //int n[3] = {(*pmb).block_size.nx1 + 2 * GSIZEI,
+  //            (*pmb).block_size.nx2 + 2 * GSIZEJ,
+  //            (*pmb).block_size.nx3 + 2 * GSIZEK};
+  int n[3] = {pz4c->mbi.nn1-1, pz4c->mbi.nn2-1, pz4c->mbi.nn3-1};
+
+  int sz = n[0] * n[1] * n[2];
+  // this could be done instead by accessing and casting the Athena vars but
+  // then it is coupled to implementation details etc.
+  Real *rho = new Real[sz], *pres = new Real[sz];
+  Real *ux = new Real[sz], *uy = new Real[sz], *uz = new Real[sz];
+
+  Real *gxx = new Real[sz], *gyy = new Real[sz], *gzz = new Real[sz];
+  Real *gxy = new Real[sz], *gxz = new Real[sz], *gyz = new Real[sz];
+
+  Real *x = new Real[n[0]];
+  Real *y = new Real[n[1]];
+  Real *z = new Real[n[2]];
+
+  // need to populate coordinates
+  for(int ix_I = 0; ix_I < n[0]; ix_I++){
+    //x[ix_I] = pco->x1v(ix_I);
+    x[ix_I] = pco->x1v(ix_I);
+  }
+
+  for(int ix_J = 0; ix_J < n[1]; ix_J++){
+    //y[ix_J] = pco->x2v(ix_J);
+    y[ix_J] = pco->x2v(ix_J);
+  }
+
+  for(int ix_K = 0; ix_K < n[2]; ix_K++){
+    //z[ix_K] = pco->x3v(ix_K);
+    z[ix_K] = pco->x3v(ix_K);
+  }
+
+  RNS_Cartesian_interpolation
+    (data, // struct containing the previously calculated solution
+     imin, // min, max idxs of Cartesian Grid in the three directions
+     n,    // 
+     n,    // total number of indices in each direction
+     x,    // x,         // Cartesian coordinates
+     y,    // y,
+     z,    // z,
+     NULL,  // alp,       // lapse
+     NULL,  // betax,   // shift vector
+     NULL, // betay,
+     NULL, // betaz,
+     NULL,  // gxx,       // metric components
+     NULL,  // gxy,
+     NULL,  // gxz,
+     NULL,  // gyy,
+     NULL,  // gyz,
+     NULL,  // gzz,
+     NULL,  // kxx,       // extrinsic curvature components
+     NULL,  // kxy,
+     NULL,  // kxz,
+     NULL,  // kyy,
+     NULL,  // kyz,
+     NULL,  // kzz
+     rho, // rho
+     NULL, // epsl - maybe for new EOS we want to take this
+     NULL, // vx
+     NULL, // vy
+     NULL, // vz
+     ux, // vx
+     uy, // vy
+     uz, // vz
+     pres  // pres
+);
+
+  Real rho_atm = rhoc*fatm;
+  Real pres_atm = k_adi*pow(rho_atm,gamma_adi);
+  int flat_ix;
+  double psi4;
+  Real r;
+//  Real vsq, Wlor;
+
+  CLOOP3(k,j,i){
+    flat_ix = i + n[0]*(j + n[1]*k);
+    r = x[i]*x[i]+y[j]*y[j]+z[k]*z[k];
+    r = std::sqrt(r);
+/*
+    vsq = vx[flat_idx]*vx[flat_idx]*gxx[flat_idx] + 
+          vy[flat_idx]*vy[flat_idx]*gyy[flat_idx] +
+          vz[flat_idx]*vz[flat_idx]*gzz[flat_idx] +
+          2.0*vx[flat_idx]*vy[flat_idx]*gxy[flat_idx] +
+          2.0*vx[flat_idx]*vz[flat_idx]*gxz[flat_idx] +
+          2.0*vy[flat_idx]*vz[flat_idx]*gyz[flat_idx];
+
+    Wlor = 1.0/(1.0-vsq);
+*/
+    if(rho[flat_ix] > rho_atm){
+        w_init(IDN,k,j,i) = w1(IDN,k,j,i) = w(IDN,k,j,i) = rho[flat_ix];
+        w_init(IPR,k,j,i) = w1(IPR,k,j,i) = w(IPR,k,j,i) = pres[flat_ix]- pres_pert*pres[flat_ix];
+        w_init(IVX,k,j,i) = w1(IVX,k,j,i) = w(IVX,k,j,i) = ux[flat_ix]  - v_pert;
+        w_init(IVY,k,j,i) = w1(IVY,k,j,i) = w(IVY,k,j,i) = uy[flat_ix]- v_pert;
+        w_init(IVZ,k,j,i) = w1(IVZ,k,j,i) = w(IVZ,k,j,i) = uz[flat_ix]- v_pert;
+    } else{
+        w_init(IDN,k,j,i) = w1(IDN,k,j,i) = w(IDN,k,j,i) = rho_atm;
+        w_init(IPR,k,j,i) = w1(IPR,k,j,i) = w(IPR,k,j,i) = pres_atm - pres_pert*pres_atm;
+        w_init(IVX,k,j,i) = w1(IVX,k,j,i) = w(IVX,k,j,i) = 0.0;
+        w_init(IVY,k,j,i) = w1(IVY,k,j,i) = w(IVY,k,j,i) = 0.0;
+        w_init(IVZ,k,j,i) = w1(IVZ,k,j,i) = w(IVZ,k,j,i) = 0.0; 
+    }
+
+  }
+
+
+  free(rho); free(pres);
+
+  free(ux); free(uy); free(uz);
+
+
+  free(x); free(y); free(z);
+
+  if(verbose)
+    std::cout << "\n\nRNS ID\n\n";
+}
+
---
+++ src/pgen/gr_rns_tov.cpp
@@ -0,0 +1,343 @@
+//========================================================================================
+// Athena++ astrophysical MHD code
+// Copyright(C) 2014 James M. Stone <jmstone@princeton.edu> and other code contributors
+// Licensed under the 3-clause BSD License, see LICENSE file for details
+//========================================================================================
+
+#include <cassert> // assert
+#include <iostream>
+
+// Athena++ headers
+#include "../athena.hpp"
+#include "../athena_arrays.hpp"
+#include "../parameter_input.hpp"
+#include "../coordinates/coordinates.hpp"
+#include "../mesh/mesh.hpp"
+#include "../z4c/z4c.hpp"
+#include "../hydro/hydro.hpp"
+#include "../eos/eos.hpp"
+#include "../field/field.hpp"
+
+
+// twopuncturesc: Stand-alone library ripped from Cactus
+#include "RNS.h"
+
+using namespace std;
+
+int RefinementCondition(MeshBlock *pmb);
+static ini_data *data;
+  Real Maxrho(MeshBlock *pmb, int iout);
+  Real L1rhodiff(MeshBlock *pmb, int iout);
+
+//========================================================================================
+//! \fn void Mesh::InitUserMeshData(ParameterInput *pin)
+//  \brief Function to initialize problem-specific data in mesh class.  Can also be used
+//  to initialize variables which are global to (and therefore can be passed to) other
+//  functions in this file.  Called in Mesh constructor.
+//========================================================================================
+
+void Mesh::InitUserMeshData(ParameterInput *pin, int res_flag)
+{
+  AllocateUserHistoryOutput(2);
+  EnrollUserHistoryOutput(0, Maxrho, "max-rho", UserHistoryOperation::max);
+  EnrollUserHistoryOutput(1, L1rhodiff, "L1rhodiff");
+
+    if (!res_flag) {     
+      string set_name = "problem";
+      //printf("BeforeSetDefault\n");
+      RNS_params_set_default();
+      //printf("AfterSetDefault\n");
+
+      string inputfile = pin->GetOrAddString("problem", "filename", "tovgamma2.par");
+
+
+      RNS_params_set_inputfile((char *) inputfile.c_str());
+
+      //printf("AfterParSetting\n");
+      data = RNS_make_initial_data();
+      //printf("AfterMakeInitData\n");
+    }
+//    if(adaptive==true)
+//      EnrollUserRefinementCondition(RefinementCondition);
+
+    return;
+}
+
+void Mesh::UserWorkAfterLoop(ParameterInput *pin, int res_flag)
+{
+  if (!res_flag)
+    RNS_finalise(data);
+  return;
+}
+
+//========================================================================================
+//! \fn void MeshBlock::ProblemGenerator(ParameterInput *pin)
+//  \brief Sets the initial conditions.
+//========================================================================================
+
+void MeshBlock::ProblemGenerator(ParameterInput *pin)
+{
+
+#ifdef Z4C_ASSERT_FINITE
+  // as a sanity check (these should be over-written)
+  pz4c->adm.psi4.Fill(NAN);
+  pz4c->adm.g_dd.Fill(NAN);
+  pz4c->adm.K_dd.Fill(NAN);
+
+  pz4c->z4c.chi.Fill(NAN);
+  pz4c->z4c.Khat.Fill(NAN);
+  pz4c->z4c.Theta.Fill(NAN);
+  pz4c->z4c.alpha.Fill(NAN);
+  pz4c->z4c.Gam_u.Fill(NAN);
+  pz4c->z4c.beta_u.Fill(NAN);
+  pz4c->z4c.g_dd.Fill(NAN);
+  pz4c->z4c.A_dd.Fill(NAN);
+
+  /*
+  pz4c->con.C.Fill(NAN);
+  pz4c->con.H.Fill(NAN);
+  pz4c->con.M.Fill(NAN);
+  pz4c->con.Z.Fill(NAN);
+  pz4c->con.M_d.Fill(NAN);
+
+  */
+#endif //Z4C_ASSERT_FINITE
+  //---------------------------------------------------------------------------
+
+
+  //Interpolate Metric quantities to VC
+  phydro->RNS_Metric(pin, pz4c->storage.adm, pz4c->storage.u, pz4c->storage.u1, data);
+
+  //Interpolate primitives to CC
+  phydro->RNS_Hydro(pin, phydro->w, phydro->w1, phydro->w_init, data);
+
+  pz4c->ADMToZ4c(pz4c->storage.adm, pz4c->storage.u);
+  pz4c->ADMToZ4c(pz4c->storage.adm, pz4c->storage.u1);
+
+    pcoord->UpdateMetric();
+
+    if(pmy_mesh->multilevel){
+        pmr->pcoarsec->UpdateMetric();
+          }
+  // Prepare index bounds
+     int il = is - NGHOST;
+     int iu = ie + NGHOST;
+     int jl = js;
+     int ju = je;
+     if (block_size.nx2 > 1) {
+        jl -= NGHOST;
+        ju += NGHOST;
+     }
+     int kl = ks;
+     int ku = ke;
+     if (block_size.nx3 > 1) {
+        kl -= NGHOST;
+        ku += NGHOST;
+     }
+                                         
+            // Initialise conserved variables
+              peos->PrimitiveToConserved(phydro->w, pfield->bcc, phydro->u, pcoord, il, iu, jl, ju, kl, ku);
+  
+                // Initialise VC matter
+                  //TODO(WC) (don't strictly need this here, will be caught in task list before used
+                    pz4c->GetMatter(pz4c->storage.mat, pz4c->storage.adm, phydro->w,pfield->bcc);
+                      pz4c->ADMConstraints(pz4c->storage.con,pz4c->storage.adm,pz4c->storage.mat,pz4c->storage.u);
+  //
+
+
+
+
+
+
+
+
+
+
+  //std::cout << "Two punctures initialized." << std::endl;
+
+  // debug!
+  /*
+  pz4c->Z4cToADM(pz4c->storage.u, pz4c->storage.adm);
+
+  pz4c->ADMConstraints(pz4c->storage.con, pz4c->storage.adm,
+                       pz4c->storage.mat, pz4c->storage.u);
+
+  */
+
+#ifdef Z4C_ASSERT_FINITE
+  pz4c->assert_is_finite_adm();
+  pz4c->assert_is_finite_con();
+  pz4c->assert_is_finite_mat();
+  pz4c->assert_is_finite_z4c();
+#endif //Z4C_ASSERT_FINITE
+
+  return;
+}
+/*
+int RefinementCondition(MeshBlock *pmb)
+{
+#ifdef Z4C_TRACKER
+  //Initial distance between one of the punctures and the edge of the full mesh, needed to
+  //calculate the box-in-box grid structure
+  Real L = pmb->pmy_mesh->pz4c_tracker->L_grid;
+  int root_lev = pmb->pmy_mesh->pz4c_tracker->root_lev;
+#ifdef DEBUG
+  printf("Root lev = %d\n", root_lev);
+#endif
+  Real xv[24];
+#ifdef DEBUG
+  printf("Max x = %g\n", pmb->block_size.x1max);
+  printf("Min x = %g\n", pmb->block_size.x1min);
+#endif
+  //Needed to calculate coordinates of vertices of a block with same center but
+  //edge of 1/8th of the original size
+  Real x1sum_sup = (5*pmb->block_size.x1max+3*pmb->block_size.x1min)/8.;
+  Real x1sum_inf = (3*pmb->block_size.x1max+5*pmb->block_size.x1min)/8.;
+  Real x2sum_sup = (5*pmb->block_size.x2max+3*pmb->block_size.x2min)/8.;
+  Real x2sum_inf = (3*pmb->block_size.x2max+5*pmb->block_size.x2min)/8.;
+  Real x3sum_sup = (5*pmb->block_size.x3max+3*pmb->block_size.x3min)/8.;
+  Real x3sum_inf = (3*pmb->block_size.x3max+5*pmb->block_size.x3min)/8.;
+
+  xv[0] = x1sum_sup;
+  xv[1] = x2sum_sup;
+  xv[2] = x3sum_sup;
+
+  xv[3] = x1sum_sup;
+  xv[4] = x2sum_sup;
+  xv[5] = x3sum_inf;
+
+  xv[6] = x1sum_sup;
+  xv[7] = x2sum_inf;
+  xv[8] = x3sum_sup;
+
+  xv[9] = x1sum_sup;
+  xv[10] = x2sum_inf;
+  xv[11] = x3sum_inf;
+
+  xv[12] = x1sum_inf;
+  xv[13] = x2sum_sup;
+  xv[14] = x3sum_sup;
+
+  xv[15] = x1sum_inf;
+  xv[16] = x2sum_sup;
+  xv[17] = x3sum_inf;
+
+  xv[18] = x1sum_inf;
+  xv[19] = x2sum_inf;
+  xv[20] = x3sum_sup;
+
+  xv[21] = x1sum_inf;
+  xv[22] = x2sum_inf;
+  xv[23] = x3sum_inf;
+
+  //Level of current block
+  int level = pmb->loc.level-root_lev;
+#ifdef DEBUG
+  printf("\n<===================================================>\n");
+  printf("L = %g\n",L);
+  printf("lev = %d\n",level);
+#endif
+  // Min distance between the two punctures
+  Real d = 1000000;
+  for (int i_punct = 0; i_punct < NPUNCT; ++i_punct) {
+    // Abs difference
+    Real diff;
+    // Max norm_inf
+    Real dmin_punct = 1000000;
+#ifdef DEBUG
+    printf("==> Punc = %d\n", i_punct);
+#endif
+    for (int i_vert = 0; i_vert < 8; ++i_vert) {
+      // Norm_inf
+      Real norm_inf = -1;
+      for (int i_diff = 0; i_diff < 3; ++ i_diff) {
+        diff = std::abs(pmb->pmy_mesh->pz4c_tracker->pos_body[i_punct].pos[i_diff] - xv[i_vert*3+i_diff]);
+#ifdef DEBUG
+        printf("======> Coordpos = %g, coordblock = %g\n",pmb->pmy_mesh->pz4c_tracker->pos_body[i_punct].pos[i_diff], xv[i_vert*3+i_diff]);
+#endif
+        if (diff > norm_inf) {
+          norm_inf = diff;
+        }
+#ifdef DEBUG
+	printf("======> Dist = %g\n", diff);
+#endif
+      }
+#ifdef DEBUG
+      printf("====> Inf norm = %g\n", norm_inf);
+#endif
+      //Calculate minimum of the distances of the 8 vertices above
+      if (dmin_punct > norm_inf) {
+        dmin_punct = norm_inf;
+      }
+    }
+#ifdef DEBUG
+    printf("====> dmin_punct = %g\n", dmin_punct);
+#endif
+    //Calculate minimum of the distances between the n punctures
+    if (d > dmin_punct) {
+      d = dmin_punct;
+    }
+  }
+#ifdef DEBUG
+  printf("Min dist = %g\n", d);
+#endif
+  Real ratio = L/d;
+  if (ratio < 1) return -1;
+  //Calculate level that the block should be in, given a box-in-box theoretical structure of the grid
+  Real th_level = std::floor(std::log2(ratio));
+#ifdef DEBUG
+  printf("Level = %d, th_level = %g\n", level, th_level);
+  printf("<===================================================>\n");
+#endif
+  if (th_level > level) {
+#ifdef DEBUG
+    printf("Refine\n");
+#endif
+    return 1;
+  } else if (th_level < level) {
+#ifdef DEBUG
+    printf("Derefine\n");
+#endif
+    return -1;
+  } else
+#ifdef DEBUG
+    printf("Do nothing\n");
+#endif
+    return 0;
+
+#else // Z4C_TRACKER
+  return 0;
+#endif // Z4C_TRACKER
+}
+*/
+Real Maxrho(MeshBlock *pmb, int iout) {
+  Real max_rho = 0.0;
+  int is = pmb->is, ie = pmb->ie, js = pmb->js, je = pmb->je, ks = pmb->ks, ke = pmb->ke;
+  AthenaArray<Real> &w = pmb->phydro->w;
+  for (int k=ks; k<=ke; k++) {
+    for (int j=js; j<=je; j++) {
+      for (int i=is; i<=ie; i++) {
+        max_rho = std::max(std::abs(w(IDN,k,j,i)), max_rho);
+      }
+    }
+  }
+  return max_rho;
+}
+
+Real L1rhodiff(MeshBlock *pmb, int iout) {
+  Real L1rho = 0.0;
+  Real vol,dx,dy,dz;
+  int is = pmb->is, ie = pmb->ie, js = pmb->js, je = pmb->je, ks = pmb->ks, ke = pmb->ke;
+  for (int k=ks; k<=ke; k++) {
+    for (int j=js; j<=je; j++) {
+      for (int i=is; i<=ie; i++) {
+        dx = pmb->pcoord->dx1v(i);
+        dy = pmb->pcoord->dx2v(j);
+        dz = pmb->pcoord->dx3v(k);
+        vol = dx*dy*dz;
+        L1rho += std::abs(pmb->phydro->w(IDN,k,j,i) - pmb->phydro->w_init(IDN,k,j,i))*vol;
+      }
+    }
+  }
+  return L1rho;
+}
