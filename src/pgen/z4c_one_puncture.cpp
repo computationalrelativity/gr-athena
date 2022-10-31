cf35135222c33ec72e8e1ce129fa8458d3442da4
Alireza <rashti.alireza@gmail.com>
2022-10-31 16:41:12 -0400

    Add refinement for one puncture problem to test the refinement method.

--- src/pgen/z4c_one_puncture.cpp
+++ src/pgen/z4c_one_puncture.cpp
@@ -8,6 +8,7 @@
 
 #include <cassert> // assert
 #include <iostream>
+#include <sstream>
 
 // Athena++ headers
 #include "../athena.hpp"
@@ -19,9 +20,10 @@
 // #include "../mesh/mesh_refinement.hpp"
 #include "../z4c/z4c.hpp"
 
-using namespace std;
+//using namespace std;
 
-int RefinementCondition(MeshBlock *pmb);
+static int RefinementCondition(MeshBlock *pmb);
+static int FDErrorApprox(MeshBlock *pmb);
 
 //========================================================================================
 //! \fn void MeshBlock::ProblemGenerator(ParameterInput *pin)
@@ -54,4 +56,63 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin)
 void MeshBlock::Z4cUserWorkInLoop() {
 
   return;
+}
+
+// 1: refines, -1: de-refines, 0: does nothing
+static int RefinementCondition(MeshBlock *pmb)
+{
+  int ret = 0;
+  ParameterInput *const pin = pmb->pmy_in;
+  
+  // finite difference error must fall less that a prescribed value.
+  if (pin->GetOrAddString("z4c","refinement","FD_error") == "FD_error")
+  {
+    ret = FDErrorApprox(pmb);
+  }
+  else
+  {
+    std::stringstream msg;
+    msg << "No such option for z4c/refinement" << std::endl;
+    ATHENA_ERROR(msg);
+  }
+  
+  return ret;
+}  
+
+// using the FD error as an approximation of the error in the meshblock.
+// if this error falls below a prescribed value, the meshblock should be refined.
+static int FDErrorApprox(MeshBlock *pmb)
+{
+  std::cout << __FUNCTION__ << std::endl;
+  
+  int ret = 0;
+  double err = 0.;
+  ParameterInput *const pin = pmb->pmy_in;
+  double ref_tol  = pin->GetOrAddReal("z4c","refinement_tol",1e-5);
+  double dref_tol = pin->GetOrAddReal("z4c","derefinement_tol",1e-6);
+  
+  // calc. err
+  err = pmb->pz4c->amr_err_L2_ddchi_pow(pmb,3);
+  
+  // if it's bigger than the specified params then refine;
+  if (err > ref_tol)
+  {
+    ret = 1.;
+    printf("err > ref-tol:   %e > %e  ==> refine me!\n",err,ref_tol);
+  }
+  else if (err < dref_tol)
+  {
+    ret = -1;
+    printf("err < deref-tol: %e < %e  ==> derefine me!\n",err,dref_tol);
+  }
+  else 
+  {
+    ret = 0;
+    printf("dref-tol <= err <= ref-tol: %e <= %e <= %e ==> I'm good!\n",dref_tol,err,ref_tol);
+  }
+  
+  fflush(stdout);
+  
+  return ret;
+  
 }
