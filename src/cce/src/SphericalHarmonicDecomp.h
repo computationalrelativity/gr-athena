3312ea9bc55952f675477a43a2b812f2f11d2bc3
Alireza <rashti.alireza@gmail.com>
2023-03-24 12:16:33 -0400

    Adapted header file.

--- src/cce/src/SphericalHarmonicDecomp.h
+++ src/cce/src/SphericalHarmonicDecomp.h
@@ -2,20 +2,18 @@
 #define SphericalHarmonicDecomp_h
 #undef USE_LEGENDRE
 
-#include "cctk.h"
-
 extern "C"
 {
 void SphericalHarmonicDecomp_Read(
      const char *name,
      const int iteration,
-     CCTK_REAL *p_time,
-     CCTK_REAL *p_Rin,
-     CCTK_REAL *p_Rout,
-     CCTK_INT *p_lmax,
-     CCTK_INT *p_nmax,
-     CCTK_REAL **p_re,
-     CCTK_REAL **p_im);
+     double *p_time,
+     double *p_Rin,
+     double *p_Rout,
+     int *p_lmax,
+     int *p_nmax,
+     double **p_re,
+     double **p_im);
 }
 
 #ifdef USE_LEGENDRE
