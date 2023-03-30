f23f719f86eb7a1786ed6560384fa1846f4da48f
Alireza <rashti.alireza@gmail.com>
2023-03-30 17:19:49 -0400

    Added a main function for an exe and hence test purposes.

---
+++ src/cce/src/main.cc
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+#define Code_mesh       const MeshBlock *mb
+class MeshBlock;
+
+void SphericalHarmonicDecomp_DumpMetric(Code_mesh);
+
+int main(void)
+{
+   printf("hi!\n");
+   SphericalHarmonicDecomp_DumpMetric(0);
+}
