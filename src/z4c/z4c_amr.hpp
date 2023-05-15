d44199a115f28d075a1ca5921f2d73724c8e4e24
Alireza <rashti.alireza@gmail.com>
2023-05-15 16:28:35 -0400

    Renamed truncation error functions. Comments.

--- src/z4c/z4c_amr.hpp
+++ src/z4c/z4c_amr.hpp
@@ -27,18 +27,18 @@ class Z4c_AMR
     
     // returning the L2 norm of: 
     // h^6 * ( (d^n fld/dx^n)^p + (d^n fld/dy^n)^p + (d^n fld/dz^n)^p )
-    Real amr_err_L2_derive_chi_pow(MeshBlock *const pmb, const int deriv_order, 
+    Real FDTruncErrorChiL2(MeshBlock *const pmb, const int deriv_order, 
                                    const int p);
 
     // returning the max err of: 
     // h^6 * ( (d^n fld/dx^n)^p + (d^n fld/dy^n)^p + (d^n fld/dz^n)^p )
-    Real amr_err_Linf_derive_chi_pow(MeshBlock *const pmb, const int deriv_order, 
+    Real FDTruncErrorChiLinf(MeshBlock *const pmb, const int deriv_order, 
                                    const int p);
 
 
    // returning the max err of: 
    // h^6 * max{|(d^n fld/dx^n)^p|, |(d^n fld/dy^n)^p|, |(d^n fld/dz^n)^p|}
-   Real amr_err_Linf_component_derive_chi_pow(MeshBlock *const pmy_block, 
+   Real FDTruncErrorChiLinfComponentWise(MeshBlock *const pmy_block, 
                                    const int deriv_order, const int p);
     
   public:
