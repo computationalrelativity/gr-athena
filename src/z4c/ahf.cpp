ef359ae3e4b669c77933bebf740b58b9bbdaa1dc
Francesco Zappa <francesco.zappa@uni-jena.de>
2023-07-06 14:30:38 +0000

    Add division by 8 pi in spin calculation, see  eq 4.37 of https://arxiv.org/pdf/1003.5015.pdf

--- src/z4c/ahf.cpp
+++ src/z4c/ahf.cpp
@@ -1087,9 +1087,9 @@ void AHF::FastFlowLoop()
     area = integrals[iarea];
     hrms = integrals[ihrms]/area;
     hmean = integrals[ihmean];
-    Sx = integrals[iSx];
-    Sy = integrals[iSy];
-    Sz = integrals[iSz];
+    Sx = integrals[iSx]/(8*PI);
+    Sy = integrals[iSy]/(8*PI);
+    Sz = integrals[iSz]/(8*PI);
 
     S = std::sqrt(SQR(Sx)+SQR(Sy)+SQR(Sz));
 
