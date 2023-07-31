5bc5d3583af291acad184cb6e962a1777bd22db9
William Cook <william.cook@uni-jena.de>
2023-07-31 14:32:01 +0200

    fix detg to 1 in mhd_lorene pgen

--- src/pgen/gr_mhd_Lorene_bns.cpp
+++ src/pgen/gr_mhd_Lorene_bns.cpp
@@ -332,7 +332,7 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin){
       phydro->w(IPR, k, j, i) = pgas;
 
       // FIXME: There needs to be a more consistent way to do this.
-      peos->ApplyPrimitiveFloors(phydro->w, k, j, i);
+//      peos->ApplyPrimitiveFloors(phydro->w, k, j, i);
       phydro->w1(IDN, k, j, i) = phydro->w(IDN, k, j, i);
       phydro->w1(IVX, k, j, i) = phydro->w(IVX, k, j, i);
       phydro->w1(IVY, k, j, i) = phydro->w(IVY, k, j, i);
@@ -417,7 +417,8 @@ pfield->bcc.ZeroClear();
           gamma_dd(2,2,i) = pz4c->ig->map3d_VC2CC(vcgamma_zz(k,j,i));
 }
    for(int i = is-1; i<=ie+1; i++){
-    Real detgamma = std::sqrt(Det3Metric(gamma_dd,i));
+//    Real detgamma = std::sqrt(Det3Metric(gamma_dd,i));
+    Real detgamma = 1.0;
 
     bxcc(k,j,i) = - ((Atot(1,k+1,j,i) - Atot(1,k-1,j,i))/(2.0*pcoord->dx3v(k)))*detgamma;
     bycc(k,j,i) =  ((Atot(0,k+1,j,i) - Atot(0,k-1,j,i))/(2.0*pcoord->dx3v(k)))*detgamma;
