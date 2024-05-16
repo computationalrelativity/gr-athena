ef912e7edb75e886c8e737f8c4053f0ae2c271f9
PeterHammond <pph5189@psu.edu>
2024-05-16 16:10:46 -0400

    Updates to opacities

--- src/m1/opacities/weakrates/m1_opacities_weakrates.hpp
+++ src/m1/opacities/weakrates/m1_opacities_weakrates.hpp
@@ -42,6 +42,9 @@ public:
     // Weakrates only works for nu_e + nu_ae + nu_x
     assert(N_SPCS==3);
 
+    // Weakrates only works for 1 group
+    assert(N_GRPS==1);
+
     // Create instance of WeakRatesNeutrinos::WeakRates
     // Set EoS from PS
     pmy_weakrates = new WeakRatesNeutrinos::WeakRates();
@@ -78,14 +81,64 @@ public:
       Real T = pmy_block->peos->GetEOS().GetTemperatureFromP(rho, press, Y);
       Real Y_e = Y[0];
 
-      ierr[0] = calc_eta(  pm1->radmat.sc_eta,   k, j, i, rho, T, Y_e);
-      ierr[1] = calc_kap_a(pm1->radmat.sc_kap_a, k, j, i, rho, T, Y_e);
-      ierr[2] = calc_kap_s(pm1->radmat.sc_kap_s, k, j, i, rho, T, Y_e);
+      Real eta_n_nue;
+      Real eta_n_nua;
+      Real eta_n_nux;
+      Real eta_e_nue;
+      Real eta_e_nua;
+      Real eta_e_nux;
+
+      ierr[0] = pmy_weakrates->NeutrinoEmission(rho, T, Y_e, eta_n_nue, eta_n_nua, eta_n_nux, eta_e_nue, eta_e_nua, eta_n_nux);
+
+      Real kap_a_n_nue;
+      Real kap_a_n_nua;
+      Real kap_a_n_nux;
+      Real kap_a_e_nue;
+      Real kap_a_e_nua;
+      Real kap_a_e_nux;
+
+      ierr[1] = pmy_weakrates->NeutrinoAbsorptionOpacity(rho, T, Y_e, kap_a_n_nue, kap_a_n_nua, kap_a_n_nux, kap_a_e_nue, kap_a_e_nua, kap_a_e_nux);
+
+      Real kap_s_n_nue;
+      Real kap_s_n_nua;
+      Real kap_s_n_nux;
+      Real kap_s_e_nue;
+      Real kap_s_e_nua;
+      Real kap_s_e_nux;
+
+      ierr[2] = pmy_weakrates->NeutrinoScatteringOpacity(rho, T, Y_e, kap_s_n_nue, kap_s_n_nua, kap_s_n_nux, kap_s_e_nue, kap_s_e_nua, kap_s_e_nux);
 
       for (int r=0; r<NUM_COEFF; ++r)
       {
         assert(!ierr[r]);
       }
+
+      // Corrections due to Kirchhoff's will replace this
+      // Number emissivity
+      pm1->radmat.sc_eta_0(0,0)(k,j,i) = eta_n_nue;
+      pm1->radmat.sc_eta_0(0,1)(k,j,i) = eta_n_nua;
+      pm1->radmat.sc_eta_0(0,2)(k,j,i) = eta_n_nux;
+
+      // Number absorption
+      pm1->radmat.sc_kap_a_0(0,0)(k,j,i) = kap_a_n_nue;
+      pm1->radmat.sc_kap_a_0(0,1)(k,j,i) = kap_a_n_nua;
+      pm1->radmat.sc_kap_a_0(0,2)(k,j,i) = kap_a_n_nux;
+
+      // Energy emissivity
+      pm1->radmat.sc_eta(0,0)(k,j,i) = eta_e_nue;
+      pm1->radmat.sc_eta(0,1)(k,j,i) = eta_e_nua;
+      pm1->radmat.sc_eta(0,2)(k,j,i) = eta_e_nux;
+
+      // Energy absorption
+      pm1->radmat.sc_kap_a(0,0)(k,j,i) = kap_a_e_nue;
+      pm1->radmat.sc_kap_a(0,1)(k,j,i) = kap_a_e_nua;
+      pm1->radmat.sc_kap_a(0,2)(k,j,i) = kap_a_e_nux;
+
+      // Energy scattering
+      pm1->radmat.sc_kap_s(0,0)(k,j,i) = kap_s_e_nue;
+      pm1->radmat.sc_kap_s(0,1)(k,j,i) = kap_s_e_nua;
+      pm1->radmat.sc_kap_s(0,2)(k,j,i) = kap_s_e_nux;
+      
     }
 
     return 0;
@@ -100,78 +153,6 @@ private:
 
   const int N_GRPS;
   const int N_SPCS;
-
-  inline int calc_eta(GroupSpeciesContainer<AT_C_sca> & eta,
-                      const int k, const int j, const int i,
-                      const Real rho,
-                      const Real T,
-                      const Real Y_e)
-  {
-    Real eta_n_nue;
-    Real eta_n_nua;
-    Real eta_n_nux;
-    Real eta_e_nue;
-    Real eta_e_nua;
-    Real eta_e_nux;
-
-    int ierr = pmy_weakrates->NeutrinoEmission(rho, T, Y_e, eta_n_nue, eta_n_nua, eta_n_nux, eta_e_nue, eta_e_nua, eta_n_nux);
-
-    for (int ix_g=0; ix_g<N_GRPS; ++ix_g) {
-      eta(ix_g,0)(k,j,i) = eta_n_nue;
-      eta(ix_g,1)(k,j,i) = eta_n_nua;
-      eta(ix_g,2)(k,j,i) = eta_n_nux;
-    }
-
-    return ierr;
-  }
-
-  inline int calc_kap_a(GroupSpeciesContainer<AT_C_sca> & kap_a,
-                        const int k, const int j, const int i,
-                        const Real rho,
-                        const Real T,
-                        const Real Y_e)
-  {
-    Real kap_a_n_nue;
-    Real kap_a_n_nua;
-    Real kap_a_n_nux;
-    Real kap_a_e_nue;
-    Real kap_a_e_nua;
-    Real kap_a_e_nux;
-
-    int ierr = pmy_weakrates->NeutrinoAbsorptionOpacity(rho, T, Y_e, kap_a_n_nue, kap_a_n_nua, kap_a_n_nux, kap_a_e_nue, kap_a_e_nua, kap_a_e_nux);
-
-    for (int ix_g=0; ix_g<N_GRPS; ++ix_g) {
-      kap_a(ix_g,0)(k,j,i) = kap_a_n_nue;
-      kap_a(ix_g,1)(k,j,i) = kap_a_n_nua;
-      kap_a(ix_g,2)(k,j,i) = kap_a_n_nux;
-    }
-
-    return ierr;
-  }
-
-  inline int calc_kap_s(GroupSpeciesContainer<AT_C_sca> & kap_s,
-                        const int k, const int j, const int i,
-                        const Real rho,
-                        const Real T,
-                        const Real Y_e)
-  {
-    Real kap_s_n_nue;
-    Real kap_s_n_nua;
-    Real kap_s_n_nux;
-    Real kap_s_e_nue;
-    Real kap_s_e_nua;
-    Real kap_s_e_nux;
-
-    int ierr = pmy_weakrates->NeutrinoScatteringOpacity(rho, T, Y_e, kap_s_n_nue, kap_s_n_nua, kap_s_n_nux, kap_s_e_nue, kap_s_e_nua, kap_s_e_nux);
-
-    for (int ix_g=0; ix_g<N_GRPS; ++ix_g) {
-      kap_s(ix_g,0)(k,j,i) = kap_s_n_nue;
-      kap_s(ix_g,1)(k,j,i) = kap_s_n_nua;
-      kap_s(ix_g,2)(k,j,i) = kap_s_n_nux;
-    }
-
-    return ierr;
-  }
 
 };
 
