0e80d2a74173231fa953b62fafac8677bea41d7d
PeterHammond <pph5189@psu.edu>
2024-07-02 16:29:10 -0400

    Weakrates fix

--- src/m1/opacities/weakrates/m1_opacities_weakrates.hpp
+++ src/m1/opacities/weakrates/m1_opacities_weakrates.hpp
@@ -79,11 +79,12 @@ public:
     {
       Real rho = pm1->hydro.sc_w_rho(k,j,i);
       Real press = pm1->hydro.sc_w_p(k,j,i);
+      Real const nb = rho/(pmy_block->peos->GetEOS().GetBaryonMass());
 
       Real Y[MAX_SPECIES] = {0.0};
       Y[0] = pm1->hydro.sc_w_Ye(k,j,i);
       
-      Real T = pmy_block->peos->GetEOS().GetTemperatureFromP(rho, press, Y);
+      Real T = pmy_block->peos->GetEOS().GetTemperatureFromP(nb, press, Y);
       Real Y_e = Y[0];
 
       Real eta_n_nue;
