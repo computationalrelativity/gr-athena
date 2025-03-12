f58d4692db9dd9f4ddbcdf49a4d8938913e66935
Boris Daszuta <boris.daszuta@uni-jena.de>
2025-03-12 12:08:22 +0100

    Dump pointwise info if weakrates fails

--- src/m1/opacities/weakrates/m1_opacities_weakrates.hpp
+++ src/m1/opacities/weakrates/m1_opacities_weakrates.hpp
@@ -135,8 +135,22 @@ public:
                                                          kap_s_e_nua,
                                                          kap_s_e_nux);
 
-      for (int r = 0; r < NUM_COEFF; ++r) {
-        assert(!ierr[r]);
+      bool is_failing_opacity = false;
+
+      for (int r = 0; r < NUM_COEFF; ++r)
+      {
+        // assert(!ierr[r]);
+        is_failing_opacity = is_failing_opacity || ierr[r];
+      }
+
+      // Dump some information when opacity calculation fails
+      if (is_failing_opacity)
+      {
+        std::ostringstream msg;
+        msg << "CalculateOpacityWeakRates failure: ";
+        pm1->StatePrintPoint(msg.str(), 0, 0, k, j, i, false);
+        pm1->StatePrintPoint(msg.str(), 0, 1, k, j, i, false);
+        pm1->StatePrintPoint(msg.str(), 0, 2, k, j, i, true);  // assert(false)
       }
 
       // Equilibrium logic ----------------------------------------------------
