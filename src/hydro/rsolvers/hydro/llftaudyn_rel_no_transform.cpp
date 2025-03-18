computationalrelativity/gr-athena

commit b7becb335eb685891fabcb99a7f2a0e01b2a7210

Allow for reconstruction based on hydro.aux (T, h)

src/hydro/rsolvers/hydro/llftaudyn_rel_no_transform.cpp
@@ -322,10 +322,47 @@ void Hydro::RiemannSolver(
     }
 #endif // DBG_COMBINED_HYDPA
 
-    Real Tl = pmy_block->peos->GetEOS().GetTemperatureFromP(nl, w_p_l_(i), Yl);
-    Real Tr = pmy_block->peos->GetEOS().GetTemperatureFromP(nr, w_p_r_(i), Yr);
-    w_hrho_l_(i) = w_rho_l_(i)*pmy_block->peos->GetEOS().GetEnthalpy(nl, Tl, Yl);
-    w_hrho_r_(i) = w_rho_r_(i)*pmy_block->peos->GetEOS().GetEnthalpy(nr, Tr, Yr);
+    Real Tl, Tr;
+    Real hl, hr;
+
+    if (pmy_block->precon->xorder_use_auxiliaries)
+    {
+      if (pmy_block->precon->xorder_use_aux_T)
+      {
+        Tl = pmy_block->phydro->al_(IX_T,i);
+        Tr = pmy_block->phydro->ar_(IX_T,i);
+      }
+      else
+      {
+        Tl = pmy_block->peos->GetEOS().GetTemperatureFromP(nl, w_p_l_(i), Yl);
+        Tr = pmy_block->peos->GetEOS().GetTemperatureFromP(nr, w_p_r_(i), Yr);
+      }
+
+      if (pmy_block->precon->xorder_use_aux_h)
+      {
+        hl = pmy_block->phydro->al_(IX_ETH,i);
+        hr = pmy_block->phydro->ar_(IX_ETH,i);
+      }
+      else
+      {
+        hl = pmy_block->peos->GetEOS().GetEnthalpy(nl, Tl, Yl);
+        hr = pmy_block->peos->GetEOS().GetEnthalpy(nr, Tr, Yr);
+      }
+
+      w_hrho_l_(i) = w_rho_l_(i) * hl;
+      w_hrho_r_(i) = w_rho_r_(i) * hr;
+    }
+    else
+    {
+      Tl = pmy_block->peos->GetEOS().GetTemperatureFromP(nl, w_p_l_(i), Yl);
+      Tr = pmy_block->peos->GetEOS().GetTemperatureFromP(nr, w_p_r_(i), Yr);
+
+      hl = pmy_block->peos->GetEOS().GetEnthalpy(nl, Tl, Yl);
+      hr = pmy_block->peos->GetEOS().GetEnthalpy(nr, Tr, Yr);
+
+      w_hrho_l_(i) = w_rho_l_(i) * hl;
+      w_hrho_r_(i) = w_rho_r_(i) * hr;
+    }
 
     // Calculate the sound speeds
     pmy_block->peos->SoundSpeedsGR(nl, Tl, w_v_u_l_(ivx-1,i), w_norm2_v_l_(i),

src/hydro/rsolvers/mhd/llftaudyn_mhd_rel_no_transform.cpp
@@ -455,14 +455,44 @@ void Hydro::RiemannSolver(
     }
   #endif // DBG_COMBINED_HYDPA
 
-    Real Tl =
-      pmy_block->peos->GetEOS().GetTemperatureFromP(nl, w_p_l_(i), Yl);
-    Real Tr =
-      pmy_block->peos->GetEOS().GetTemperatureFromP(nr, w_p_r_(i), Yr);
-    w_hrho_l_(i) =
-      w_rho_l_(i) * pmy_block->peos->GetEOS().GetEnthalpy(nl, Tl, Yl);
-    w_hrho_r_(i) =
-      w_rho_r_(i) * pmy_block->peos->GetEOS().GetEnthalpy(nr, Tr, Yr);
+    Real Tl, Tr;
+    Real hl, hr;
+
+    if (pmy_block->precon->xorder_use_auxiliaries)
+    {
+      if (pmy_block->precon->xorder_use_aux_T)
+      {
+        Tl = pmy_block->phydro->al_(IX_T,i);
+        Tr = pmy_block->phydro->ar_(IX_T,i);
+      }
+      else
+      {
+        Tl = pmy_block->peos->GetEOS().GetTemperatureFromP(nl, w_p_l_(i), Yl);
+        Tr = pmy_block->peos->GetEOS().GetTemperatureFromP(nr, w_p_r_(i), Yr);
+      }
+
+      if (pmy_block->precon->xorder_use_aux_h)
+      {
+        hl = pmy_block->phydro->al_(IX_ETH,i);
+        hr = pmy_block->phydro->ar_(IX_ETH,i);
+      }
+      else
+      {
+        hl = pmy_block->peos->GetEOS().GetEnthalpy(nl, Tl, Yl);
+        hr = pmy_block->peos->GetEOS().GetEnthalpy(nr, Tr, Yr);
+      }
+
+      w_hrho_l_(i) = w_rho_l_(i) * hl;
+      w_hrho_r_(i) = w_rho_r_(i) * hr;
+    }
+    else
+    {
+      Tl = pmy_block->peos->GetEOS().GetTemperatureFromP(nl, w_p_l_(i), Yl);
+      Tr = pmy_block->peos->GetEOS().GetTemperatureFromP(nr, w_p_r_(i), Yr);
+
+      w_hrho_l_(i) = w_rho_l_(i) * hl;
+      w_hrho_r_(i) = w_rho_r_(i) * hr;
+    }
 
     // Calculate the wave speeds
     pmy_block->peos->FastMagnetosonicSpeedsGR(

src/hydro/calculate_fluxes.cpp
@@ -372,6 +372,13 @@ void Hydro::CalculateFluxesCombined(AthenaArray<Real> &w,
                                      ps->rl_, ps->rr_,
                                      k, j, il, iu);
 
+    if (pr->xorder_use_auxiliaries)
+    {
+      pr->ReconstructHydroAuxiliariesX1_(rv, derived_ms,
+                                         al_, ar_,
+                                         k, j, il, iu);
+    }
+
     if (pr->xorder_use_fb)
     {
       pr->ReconstructPrimitivesX1_(r_rv, w, r_wl_, r_wr_,
@@ -382,6 +389,13 @@ void Hydro::CalculateFluxesCombined(AthenaArray<Real> &w,
                                        ps->r_rl_, ps->r_rr_,
                                        k, j, il, iu);
 
+      if (pr->xorder_use_auxiliaries)
+      {
+        pr->ReconstructHydroAuxiliariesX1_(r_rv, derived_ms,
+                                           r_al_, r_ar_,
+                                           k, j, il, iu);
+      }
+
       FallbackInadmissiblePrimitiveX1_(wl_, wr_,
                                        r_wl_, r_wr_,
                                        il, iu);
@@ -389,9 +403,11 @@ void Hydro::CalculateFluxesCombined(AthenaArray<Real> &w,
                                         ps->r_rl_, ps->r_rr_,
                                         il, iu);
 
+      // TODO:
+      // need fallback for auxiliaries...
+
       // ps->ApplySpeciesLimits(ps->rl_, il, iu);
       // ps->ApplySpeciesLimits(ps->rr_, il, iu);
-
     }
     else
     {
@@ -406,6 +422,10 @@ void Hydro::CalculateFluxesCombined(AthenaArray<Real> &w,
       FloorPrimitiveX1_(wl_, wr_,
                         k, j, il, iu);
 #endif
+
+      if (pr->xorder_use_auxiliaries)
+        LimitAuxiliariesX1_(al_, ar_, il, iu);
+
     }
 
     pmb->pcoord->CenterWidth1(k, j, il, iu, dxw_);
@@ -447,6 +467,13 @@ void Hydro::CalculateFluxesCombined(AthenaArray<Real> &w,
                                        ps->rl_, ps->rr_,
                                        k, jl-1, il, iu);
 
+      if (pr->xorder_use_auxiliaries)
+      {
+        pr->ReconstructHydroAuxiliariesX2_(rv, derived_ms,
+                                           al_, ar_,
+                                           k, jl-1, il, iu);
+      }
+
       if (pr->xorder_use_fb)
       {
         pr->ReconstructPrimitivesX2_(r_rv, w,
@@ -458,6 +485,13 @@ void Hydro::CalculateFluxesCombined(AthenaArray<Real> &w,
         pr->ReconstructPassiveScalarsX2_(r_rv, r,
                                          ps->r_rl_, ps->r_rr_,
                                          k, jl-1, il, iu);
+
+        if (pr->xorder_use_auxiliaries)
+        {
+          pr->ReconstructHydroAuxiliariesX2_(r_rv, derived_ms,
+                                             r_al_, r_ar_,
+                                             k, jl-1, il, iu);
+        }
 
         FallbackInadmissiblePrimitiveX2_(wl_, wr_,
                                          r_wl_, r_wr_,
@@ -483,6 +517,10 @@ void Hydro::CalculateFluxesCombined(AthenaArray<Real> &w,
         FloorPrimitiveX2_(wl_, wr_,
                           k, jl-1, il, iu);
 #endif
+
+        if (pr->xorder_use_auxiliaries)
+          LimitAuxiliariesX2_(al_, ar_, il, iu);
+
       }
 
 
@@ -499,6 +537,13 @@ void Hydro::CalculateFluxesCombined(AthenaArray<Real> &w,
                                          ps->rlb_, ps->rr_,
                                          k, j, il, iu);
 
+        if (pr->xorder_use_auxiliaries)
+        {
+          pr->ReconstructHydroAuxiliariesX2_(rv, derived_ms,
+                                             alb_, ar_,
+                                             k, j, il, iu);
+        }
+
         if (pr->xorder_use_fb)
         {
           pr->ReconstructPrimitivesX2_(r_rv, w,
@@ -510,6 +555,13 @@ void Hydro::CalculateFluxesCombined(AthenaArray<Real> &w,
           pr->ReconstructPassiveScalarsX2_(r_rv, r,
                                            ps->r_rlb_, ps->r_rr_,
                                            k, j, il, iu);
+
+          if (pr->xorder_use_auxiliaries)
+          {
+            pr->ReconstructHydroAuxiliariesX2_(r_rv, derived_ms,
+                                               r_alb_, r_ar_,
+                                               k, j, il, iu);
+          }
 
           FallbackInadmissiblePrimitiveX2_(wlb_, wr_,
                                            r_wlb_, r_wr_,
@@ -534,6 +586,10 @@ void Hydro::CalculateFluxesCombined(AthenaArray<Real> &w,
 #else
           FloorPrimitiveX2_(wlb_, wr_, k, j, il, iu);
 #endif
+
+          if (pr->xorder_use_auxiliaries)
+            LimitAuxiliariesX2_(alb_, ar_, il, iu);
+
         }
 
         pmb->pcoord->CenterWidth2(k, j, il, iu, dxw_);
@@ -556,12 +612,23 @@ void Hydro::CalculateFluxesCombined(AthenaArray<Real> &w,
 #if NSCALARS > 0
         ps->rl_.SwapAthenaArray(ps->rlb_);
 #endif
+
+        if (pr->xorder_use_auxiliaries)
+        {
+          al_.SwapAthenaArray(alb_);
+        }
+
         if (pr->xorder_use_fb)
         {
           r_wl_.SwapAthenaArray(r_wlb_);
 #if NSCALARS > 0
           ps->r_rl_.SwapAthenaArray(ps->r_rlb_);
 #endif
+
+          if (pr->xorder_use_auxiliaries)
+          {
+            r_al_.SwapAthenaArray(r_alb_);
+          }
         }
       }
     }
@@ -589,6 +656,13 @@ void Hydro::CalculateFluxesCombined(AthenaArray<Real> &w,
                                        ps->rl_, ps->rr_,
                                        kl-1, j, il, iu);
 
+      if (pr->xorder_use_auxiliaries)
+      {
+        pr->ReconstructHydroAuxiliariesX3_(rv, derived_ms,
+                                           al_, ar_,
+                                           kl-1, j, il, iu);
+      }
+
       if (pr->xorder_use_fb)
       {
         pr->ReconstructPrimitivesX3_(r_rv, w,
@@ -600,6 +674,13 @@ void Hydro::CalculateFluxesCombined(AthenaArray<Real> &w,
         pr->ReconstructPassiveScalarsX3_(r_rv, r,
                                          ps->r_rl_, ps->r_rr_,
                                          kl-1, j, il, iu);
+
+        if (pr->xorder_use_auxiliaries)
+        {
+          pr->ReconstructHydroAuxiliariesX3_(r_rv, derived_ms,
+                                             r_al_, r_ar_,
+                                             kl-1, j, il, iu);
+        }
 
         FallbackInadmissiblePrimitiveX3_(wl_, wr_,
                                          r_wl_, r_wr_,
@@ -624,6 +705,10 @@ void Hydro::CalculateFluxesCombined(AthenaArray<Real> &w,
 #else
         FloorPrimitiveX3_(wl_, wr_, kl-1, j, il, iu);
 #endif
+
+        if (pr->xorder_use_auxiliaries)
+          LimitAuxiliariesX3_(al_, ar_, il, iu);
+
       }
 
 
@@ -639,6 +724,13 @@ void Hydro::CalculateFluxesCombined(AthenaArray<Real> &w,
                                          ps->rlb_, ps->rr_,
                                          k, j, il, iu);
 
+        if (pr->xorder_use_auxiliaries)
+        {
+          pr->ReconstructHydroAuxiliariesX3_(rv, derived_ms,
+                                             alb_, ar_,
+                                             k, j, il, iu);
+        }
+
         if (pr->xorder_use_fb)
         {
           pr->ReconstructPrimitivesX3_(r_rv, w,
@@ -650,6 +742,13 @@ void Hydro::CalculateFluxesCombined(AthenaArray<Real> &w,
           pr->ReconstructPassiveScalarsX3_(r_rv, r,
                                            ps->r_rlb_, ps->r_rr_,
                                            k, j, il, iu);
+
+          if (pr->xorder_use_auxiliaries)
+          {
+            pr->ReconstructHydroAuxiliariesX3_(r_rv, derived_ms,
+                                               r_alb_, r_ar_,
+                                               k, j, il, iu);
+          }
 
           FallbackInadmissiblePrimitiveX3_(wlb_, wr_,
                                            r_wlb_, r_wr_,
@@ -675,6 +774,10 @@ void Hydro::CalculateFluxesCombined(AthenaArray<Real> &w,
           FloorPrimitiveX3_(wlb_, wr_,
                             k, j, il, iu);
 #endif
+
+          if (pr->xorder_use_auxiliaries)
+            LimitAuxiliariesX3_(alb_, ar_, il, iu);
+
         }
 
         pmb->pcoord->CenterWidth3(k, j, il, iu, dxw_);
@@ -697,12 +800,22 @@ void Hydro::CalculateFluxesCombined(AthenaArray<Real> &w,
 #if NSCALARS > 0
         ps->rl_.SwapAthenaArray(ps->rlb_);
 #endif
+
+        if (pr->xorder_use_auxiliaries)
+        {
+          al_.SwapAthenaArray(alb_);
+        }
+
         if (pr->xorder_use_fb)
         {
           r_wl_.SwapAthenaArray(r_wlb_);
 #if NSCALARS > 0
           ps->r_rl_.SwapAthenaArray(ps->r_rlb_);
 #endif
+          if (pr->xorder_use_auxiliaries)
+          {
+            r_al_.SwapAthenaArray(r_alb_);
+          }
         }
       }
     }

src/hydro/hydro.cpp
@@ -97,11 +97,25 @@ Hydro::Hydro(MeshBlock *pmb, ParameterInput *pin) :
   wr_.NewAthenaArray(NWAVE, nc1);
   wlb_.NewAthenaArray(NWAVE, nc1);
 
+  if (pmy_block->precon->xorder_use_auxiliaries)
+  {
+    al_.NewAthenaArray(NDRV_HYDRO, nc1);
+    ar_.NewAthenaArray(NDRV_HYDRO, nc1);
+    alb_.NewAthenaArray(NDRV_HYDRO, nc1);
+  }
+
   if (pmy_block->precon->xorder_use_fb)
   {
     r_wl_.NewAthenaArray(NWAVE, nc1);
     r_wr_.NewAthenaArray(NWAVE, nc1);
     r_wlb_.NewAthenaArray(NWAVE, nc1);
+
+    if (pmy_block->precon->xorder_use_auxiliaries)
+    {
+      r_al_.NewAthenaArray(NDRV_HYDRO, nc1);
+      r_ar_.NewAthenaArray(NDRV_HYDRO, nc1);
+      r_alb_.NewAthenaArray(NDRV_HYDRO, nc1);
+    }
   }
   else
   {

src/hydro/hydro.hpp
@@ -615,6 +615,102 @@ public:
   }
 #endif
 
+  inline void LimitAuxiliariesX1_(
+    AA & al_,
+    AA & ar_,
+    const int il, const int iu)
+  {
+    // Either 0 or 1, depending on l/r convention
+    static const int I = DGB_RECON_X1_OFFSET;
+
+    EquationOfState *peos = pmy_block->peos;
+    Reconstruction  *precon = pmy_block->precon;
+
+    // N.B. indicial range is bumped left to account for reconstruction
+    // convention in X1
+#if USETM
+    if (precon->xorder_use_aux_T)
+    #pragma omp simd
+    for (int i=il-1; i<=iu; ++i)
+    {
+      peos->GetEOS().ApplyTemperatureLimits(al_(IX_T,i+I));
+      peos->GetEOS().ApplyTemperatureLimits(ar_(IX_T,i));
+    }
+
+    if (precon->xorder_use_aux_h)
+    {
+      const Real min_ETH = peos->GetEOS().GetMinimumEnthalpy();
+      #pragma omp simd
+      for (int i=il-1; i<=iu; ++i)
+      {
+        al_(IX_ETH,i+I) = std::max(al_(IX_ETH,i+I), min_ETH);
+        ar_(IX_ETH,i  ) = std::max(ar_(IX_ETH,i  ), min_ETH);
+      }
+    }
+#endif // USETM
+  }
+
+  inline void LimitAuxiliariesX2_(
+    AA & al_,
+    AA & ar_,
+    const int il, const int iu)
+  {
+    EquationOfState *peos = pmy_block->peos;
+    Reconstruction  *precon = pmy_block->precon;
+
+#if USETM
+    if (precon->xorder_use_aux_T)
+    #pragma omp simd
+    for (int i=il; i<=iu; ++i)
+    {
+      peos->GetEOS().ApplyTemperatureLimits(al_(IX_T,i));
+      peos->GetEOS().ApplyTemperatureLimits(ar_(IX_T,i));
+    }
+
+    if (precon->xorder_use_aux_h)
+    {
+      const Real min_ETH = peos->GetEOS().GetMinimumEnthalpy();
+      #pragma omp simd
+      for (int i=il; i<=iu; ++i)
+      {
+        al_(IX_ETH,i) = std::max(al_(IX_ETH,i), min_ETH);
+        ar_(IX_ETH,i) = std::max(ar_(IX_ETH,i), min_ETH);
+      }
+    }
+#endif // USETM
+  }
+
+  inline void LimitAuxiliariesX3_(
+    AA & al_,
+    AA & ar_,
+    const int il, const int iu)
+  {
+    EquationOfState *peos = pmy_block->peos;
+    Reconstruction  *precon = pmy_block->precon;
+
+#if USETM
+    if (precon->xorder_use_aux_T)
+    #pragma omp simd
+    for (int i=il; i<=iu; ++i)
+    {
+      peos->GetEOS().ApplyTemperatureLimits(al_(IX_T,i));
+      peos->GetEOS().ApplyTemperatureLimits(ar_(IX_T,i));
+    }
+
+    if (precon->xorder_use_aux_h)
+    {
+      const Real min_ETH = peos->GetEOS().GetMinimumEnthalpy();
+      #pragma omp simd
+      for (int i=il; i<=iu; ++i)
+      {
+        al_(IX_ETH,i) = std::max(al_(IX_ETH,i), min_ETH);
+        ar_(IX_ETH,i) = std::max(ar_(IX_ETH,i), min_ETH);
+      }
+    }
+
+#endif // USETM
+  }
+
  private:
   AA dt1_, dt2_, dt3_;  // scratch arrays used in NewTimeStep
   // scratch space used to compute fluxes
@@ -623,6 +719,10 @@ public:
   AA wl_, wr_, wlb_;
   AA r_wl_, r_wr_, r_wlb_;
   AA rl_, rr_, rlb_;
+
+  // reconstruction for auxiliaries
+  AA al_, ar_, alb_;
+  AA r_al_, r_ar_, r_alb_;
 
   AA dflx_;
 

src/reconstruct/reconstruction.cpp
@@ -137,6 +137,17 @@ Reconstruction::Reconstruction(MeshBlock *pmb, ParameterInput *pin)
       "time", "xorder_use_fb_unphysical", false);
   }
 
+  xorder_use_auxiliaries = pin->GetOrAddBoolean(
+    "time", "xorder_use_auxiliaries", false
+  );
+
+  xorder_use_aux_T = pin->GetOrAddBoolean(
+    "time", "xorder_use_aux_T", false
+  );
+
+  xorder_use_aux_h = pin->GetOrAddBoolean(
+    "time", "xorder_use_aux_h", false
+  );
 
   // Check for incompatible choices with broader solver configuration
   // --------------------------------

src/reconstruct/reconstruction.hpp
@@ -44,6 +44,10 @@ class Reconstruction {
   bool xorder_use_fb;                     // try order reduction
   bool xorder_use_fb_unphysical = false;  // try energy conditions
 
+  bool xorder_use_auxiliaries;            // reconstruct derived quantities?
+  bool xorder_use_aux_T;                  // reconstruct temperature?
+  bool xorder_use_aux_h;                  // reconstruct enthalpy?
+
   bool characteristic_projection; // reconstruct on characteristic or primitive hydro vars
   bool uniform[3], curvilinear[2];
   // (Cartesian reconstruction formulas are used for x3 azimuthal coordinate in both
@@ -212,6 +216,22 @@ class Reconstruction {
     }
   }
 
+  inline void ReconstructHydroAuxiliariesX1_(
+    ReconstructionVariant rv,
+    AthenaArray<Real> & z,
+    AthenaArray<Real> & zl_,
+    AthenaArray<Real> & zr_,
+    const int k, const int j, const int il, const int iu)
+  {
+    // wl_ populated at i+1 on Recon. call
+    for (int n=0; n<NDRV_HYDRO; ++n)
+    {
+      if (((n == IX_T) && xorder_use_aux_T) ||
+           (n == IX_ETH && xorder_use_aux_h))
+        ReconstructFieldX1(rv, z, zl_, zr_, n, n, k, j, il-1, iu);
+    }
+  }
+
   inline void ReconstructPrimitivesX2_(
     ReconstructionVariant rv,
     AthenaArray<Real> & z,
@@ -251,6 +271,21 @@ class Reconstruction {
     }
   }
 
+  inline void ReconstructHydroAuxiliariesX2_(
+    ReconstructionVariant rv,
+    AthenaArray<Real> & z,
+    AthenaArray<Real> & zl_,
+    AthenaArray<Real> & zr_,
+    const int k, const int j, const int il, const int iu)
+  {
+    for (int n=0; n<NDRV_HYDRO; ++n)
+    {
+      if (((n == IX_T) && xorder_use_aux_T) ||
+           (n == IX_ETH && xorder_use_aux_h))
+        ReconstructFieldX2(rv, z, zl_, zr_, n, n, k, j, il, iu);
+    }
+  }
+
   inline void ReconstructPrimitivesX3_(
     ReconstructionVariant rv,
     AthenaArray<Real> & z,
@@ -287,6 +322,21 @@ class Reconstruction {
     for (int n=0; n<NSCALARS; ++n)
     {
       ReconstructFieldX3(rv, z, zl_, zr_, n, n, k, j, il, iu);
+    }
+  }
+
+  inline void ReconstructHydroAuxiliariesX3_(
+    ReconstructionVariant rv,
+    AthenaArray<Real> & z,
+    AthenaArray<Real> & zl_,
+    AthenaArray<Real> & zr_,
+    const int k, const int j, const int il, const int iu)
+  {
+    for (int n=0; n<NDRV_HYDRO; ++n)
+    {
+      if (((n == IX_T) && xorder_use_aux_T) ||
+           (n == IX_ETH && xorder_use_aux_h))
+        ReconstructFieldX3(rv, z, zl_, zr_, n, n, k, j, il, iu);
     }
   }
 

commit 489aa10de624b140eb5d746710cee844f6dbe83b

Fix incorrect conditional chain introduced in b7becb335eb685891fabcb99a7f2a0e01b2a7210

src/hydro/rsolvers/hydro/llftaudyn_rel_no_transform.cpp
@@ -325,44 +325,30 @@ void Hydro::RiemannSolver(
     Real Tl, Tr;
     Real hl, hr;
 
-    if (pmy_block->precon->xorder_use_auxiliaries)
+    if (pmy_block->precon->xorder_use_aux_T)
     {
-      if (pmy_block->precon->xorder_use_aux_T)
-      {
-        Tl = pmy_block->phydro->al_(IX_T,i);
-        Tr = pmy_block->phydro->ar_(IX_T,i);
-      }
-      else
-      {
-        Tl = pmy_block->peos->GetEOS().GetTemperatureFromP(nl, w_p_l_(i), Yl);
-        Tr = pmy_block->peos->GetEOS().GetTemperatureFromP(nr, w_p_r_(i), Yr);
-      }
-
-      if (pmy_block->precon->xorder_use_aux_h)
-      {
-        hl = pmy_block->phydro->al_(IX_ETH,i);
-        hr = pmy_block->phydro->ar_(IX_ETH,i);
-      }
-      else
-      {
-        hl = pmy_block->peos->GetEOS().GetEnthalpy(nl, Tl, Yl);
-        hr = pmy_block->peos->GetEOS().GetEnthalpy(nr, Tr, Yr);
-      }
-
-      w_hrho_l_(i) = w_rho_l_(i) * hl;
-      w_hrho_r_(i) = w_rho_r_(i) * hr;
+      Tl = pmy_block->phydro->al_(IX_T,i);
+      Tr = pmy_block->phydro->ar_(IX_T,i);
     }
     else
     {
       Tl = pmy_block->peos->GetEOS().GetTemperatureFromP(nl, w_p_l_(i), Yl);
       Tr = pmy_block->peos->GetEOS().GetTemperatureFromP(nr, w_p_r_(i), Yr);
+    }
 
+    if (pmy_block->precon->xorder_use_aux_h)
+    {
+      hl = pmy_block->phydro->al_(IX_ETH,i);
+      hr = pmy_block->phydro->ar_(IX_ETH,i);
+    }
+    else
+    {
       hl = pmy_block->peos->GetEOS().GetEnthalpy(nl, Tl, Yl);
       hr = pmy_block->peos->GetEOS().GetEnthalpy(nr, Tr, Yr);
-
-      w_hrho_l_(i) = w_rho_l_(i) * hl;
-      w_hrho_r_(i) = w_rho_r_(i) * hr;
     }
+
+    w_hrho_l_(i) = w_rho_l_(i) * hl;
+    w_hrho_r_(i) = w_rho_r_(i) * hr;
 
     // Calculate the sound speeds
     pmy_block->peos->SoundSpeedsGR(nl, Tl, w_v_u_l_(ivx-1,i), w_norm2_v_l_(i),

src/hydro/rsolvers/mhd/llftaudyn_mhd_rel_no_transform.cpp
@@ -458,41 +458,30 @@ void Hydro::RiemannSolver(
     Real Tl, Tr;
     Real hl, hr;
 
-    if (pmy_block->precon->xorder_use_auxiliaries)
+    if (pmy_block->precon->xorder_use_aux_T)
     {
-      if (pmy_block->precon->xorder_use_aux_T)
-      {
-        Tl = pmy_block->phydro->al_(IX_T,i);
-        Tr = pmy_block->phydro->ar_(IX_T,i);
-      }
-      else
-      {
-        Tl = pmy_block->peos->GetEOS().GetTemperatureFromP(nl, w_p_l_(i), Yl);
-        Tr = pmy_block->peos->GetEOS().GetTemperatureFromP(nr, w_p_r_(i), Yr);
-      }
-
-      if (pmy_block->precon->xorder_use_aux_h)
-      {
-        hl = pmy_block->phydro->al_(IX_ETH,i);
-        hr = pmy_block->phydro->ar_(IX_ETH,i);
-      }
-      else
-      {
-        hl = pmy_block->peos->GetEOS().GetEnthalpy(nl, Tl, Yl);
-        hr = pmy_block->peos->GetEOS().GetEnthalpy(nr, Tr, Yr);
-      }
-
-      w_hrho_l_(i) = w_rho_l_(i) * hl;
-      w_hrho_r_(i) = w_rho_r_(i) * hr;
+      Tl = pmy_block->phydro->al_(IX_T,i);
+      Tr = pmy_block->phydro->ar_(IX_T,i);
     }
     else
     {
       Tl = pmy_block->peos->GetEOS().GetTemperatureFromP(nl, w_p_l_(i), Yl);
       Tr = pmy_block->peos->GetEOS().GetTemperatureFromP(nr, w_p_r_(i), Yr);
-
-      w_hrho_l_(i) = w_rho_l_(i) * hl;
-      w_hrho_r_(i) = w_rho_r_(i) * hr;
     }
+
+    if (pmy_block->precon->xorder_use_aux_h)
+    {
+      hl = pmy_block->phydro->al_(IX_ETH,i);
+      hr = pmy_block->phydro->ar_(IX_ETH,i);
+    }
+    else
+    {
+      hl = pmy_block->peos->GetEOS().GetEnthalpy(nl, Tl, Yl);
+      hr = pmy_block->peos->GetEOS().GetEnthalpy(nr, Tr, Yr);
+    }
+
+    w_hrho_l_(i) = w_rho_l_(i) * hl;
+    w_hrho_r_(i) = w_rho_r_(i) * hr;
 
     // Calculate the wave speeds
     pmy_block->peos->FastMagnetosonicSpeedsGR(