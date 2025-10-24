1cbb2de3a69186259dd5c20b9f1625d2623fb4e6
Boris Daszuta <boris.daszuta@uni-jena.de>
2025-10-24 20:52:33 +0200

    Off-centre single puncture via: problem/os_x1,2,3=...

--- src/z4c/one_puncture_z4c.cpp
+++ src/z4c/one_puncture_z4c.cpp
@@ -25,13 +25,22 @@ void Z4c::ADMOnePuncture(ParameterInput *pin, AthenaArray<Real> & u_adm)
   SetADMAliases(u_adm, adm);
   Real ADM_mass = pin->GetOrAddReal("problem", "punc_ADM_mass", 1.);
 
+  // offset
+  const Real os_x1 = pin->GetOrAddReal("problem", "os_x1", 0.0);
+  const Real os_x2 = pin->GetOrAddReal("problem", "os_x2", 0.0);
+  const Real os_x3 = pin->GetOrAddReal("problem", "os_x3", 0.0);
+
   // Flat spacetime
   ADMMinkowski(u_adm);
 
   GLOOP2(k,j) {
     // Isotropic radius
     GLOOP1(i) {
-      r(i) = std::sqrt(SQR(mbi.x1(i)) + SQR(mbi.x2(j)) + SQR(mbi.x3(k)));
+      r(i) = std::sqrt(
+        SQR(mbi.x1(i) - os_x1) +
+        SQR(mbi.x2(j) - os_x2) +
+        SQR(mbi.x3(k) - os_x3)
+      );
     }
 
     GLOOP1(i) {
