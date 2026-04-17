9b468a6d378db7cf1c85d265c9ee137a8a0402e5
Boris Daszuta <boris.daszuta@uni-jena.de>
2026-04-17 17:00:18 +0200

    Port: 14a11fb of feature/bns_pgen_use_energy; "add energy inversion to cold_piecewise_polytrope"

--- src/z4c/primitive/cold_piecewise_polytrope.cpp
+++ src/z4c/primitive/cold_piecewise_polytrope.cpp
@@ -1,73 +1,87 @@
 //! \file cold_piecewise_polytrope.cpp
 //  \brief Implementation of ColdPiecewisePolytrope
 
+#include "cold_piecewise_polytrope.hpp"
+
 #include <cassert>
 #include <cmath>
 #include <cstdio>
 #include <stdexcept>
 
 #include "unit_system.hpp"
-#include "cold_piecewise_polytrope.hpp"
 
 using namespace Primitive;
 using namespace std;
 
-ColdPiecewisePolytrope::ColdPiecewisePolytrope():
-  PiecewisePolytrope() {
+ColdPiecewisePolytrope::ColdPiecewisePolytrope() : PiecewisePolytrope()
+{
   eos_units = &Nuclear;
-  T = 0.0;
-  mb = 1.0;
+  T         = 0.0;
+  mb        = 1.0;
 }
 
-ColdPiecewisePolytrope::~ColdPiecewisePolytrope() {}
+ColdPiecewisePolytrope::~ColdPiecewisePolytrope()
+{
+}
 
-
-Real ColdPiecewisePolytrope::Pressure(Real n) {
-  assert (IsInitialized());
+Real ColdPiecewisePolytrope::Pressure(Real n)
+{
+  assert(IsInitialized());
   int p = FindPiece(n);
-  return GetColdPressure(n, p) + n*T;
+  return GetColdPressure(n, p) + n * T;
 }
 
-Real ColdPiecewisePolytrope::Energy(Real n) {
-  assert (IsInitialized());
+Real ColdPiecewisePolytrope::Energy(Real n)
+{
+  assert(IsInitialized());
   int p = FindPiece(n);
-  return GetColdEnergy(n, p) + n*T/(gamma_thermal - 1.0);
+  return GetColdEnergy(n, p) + n * T / (gamma_thermal - 1.0);
 }
 
-Real ColdPiecewisePolytrope::dPdn(Real n) {
-  assert (IsInitialized());
+Real ColdPiecewisePolytrope::dPdn(Real n)
+{
+  assert(IsInitialized());
   int p = FindPiece(n);
-  return pressure_pieces[p] * gamma_pieces[p]
-    * std::pow(n, gamma_pieces[p]-1)
-    / std::pow(density_pieces[p], gamma_pieces[p])
-    + T;
+  return pressure_pieces[p] * gamma_pieces[p] *
+           std::pow(n, gamma_pieces[p] - 1) /
+           std::pow(density_pieces[p], gamma_pieces[p]) +
+         T;
 }
 
-Real ColdPiecewisePolytrope::SpecificInternalEnergy(Real n) {
-  assert (IsInitialized());
-  int p = FindPiece(n);
-  Real eps_cold = GetColdEnergy(n, p)/(n*mb) - 1.0;
-  return eps_cold + T/(mb*(gamma_thermal - 1.0));
+Real ColdPiecewisePolytrope::SpecificInternalEnergy(Real n)
+{
+  assert(IsInitialized());
+  int p         = FindPiece(n);
+  Real eps_cold = GetColdEnergy(n, p) / (n * mb) - 1.0;
+  return eps_cold + T / (mb * (gamma_thermal - 1.0));
 }
 
-Real ColdPiecewisePolytrope::Y(Real n, int iy) {
+Real ColdPiecewisePolytrope::Y(Real n, int iy)
+{
   throw std::logic_error("ColdPiecewisePolytrope::Y not implemented");
 }
 
-Real ColdPiecewisePolytrope::Enthalpy(Real n) {
-  assert (IsInitialized());
+Real ColdPiecewisePolytrope::Enthalpy(Real n)
+{
+  assert(IsInitialized());
   int p = FindPiece(n);
-  return (GetColdEnergy(n, p) + GetColdPressure(n, p))/n + gamma_thermal/(gamma_thermal - 1.0)*T;
+  return (GetColdEnergy(n, p) + GetColdPressure(n, p)) / n +
+         gamma_thermal / (gamma_thermal - 1.0) * T;
 }
 
-int ColdPiecewisePolytrope::FindPieceFromP(Real P) const {
+int ColdPiecewisePolytrope::FindPieceFromP(Real P) const
+{
   // Throw an error if the polytrope hasn't been initialized yet.
-  if (!initialized) {
-    throw std::runtime_error("ColdPiecewisePolytrope::FindPieceFromP - EOS not initialized.");
+  if (!initialized)
+  {
+    throw std::runtime_error(
+      "ColdPiecewisePolytrope::FindPieceFromP - EOS not initialized.");
   }
 
-  for (int i = 0; i < n_pieces-1; ++i) {
-    if (P < pressure_pieces[i+1]) {
+  for (int i = 0; i < n_pieces - 1; ++i)
+  {
+    if (P < pressure_pieces[i + 1])
+    {
       return i;
     }
   }
@@ -75,12 +89,84 @@ int ColdPiecewisePolytrope::FindPieceFromP(Real P) const {
   return n_pieces - 1;
 }
 
-Real ColdPiecewisePolytrope::DensityFromPressure(Real P) {
-  assert (IsInitialized());
-  if (T > 1e-10) {
-    throw std::runtime_error("ColdPiecewisePolytrope::DensityFromPressure only implemented for T=0");
+int ColdPiecewisePolytrope::FindPieceFromE(Real e) const
+{
+  // Throw an error if the polytrope hasn't been initialized yet.
+  if (!initialized)
+  {
+    throw std::runtime_error(
+      "ColdPiecewisePolytrope::FindPieceFromE - EOS not initialized.");
+  }
+
+  for (int i = 0; i < n_pieces - 1; ++i)
+  {
+    if (e < mb * density_pieces[i + 1] * (1.0 + eps_pieces[i + 1]))
+    {
+      return i;
+    }
+  }
+
+  return n_pieces - 1;
+}
+
+Real ColdPiecewisePolytrope::DensityFromPressure(Real P)
+{
+  assert(IsInitialized());
+  if (T > 1e-10)
+  {
+    throw std::runtime_error(
+      "ColdPiecewisePolytrope::DensityFromPressure only implemented for T=0");
   }
 
   int p = FindPieceFromP(P);
-  return density_pieces[p] * std::pow(P/pressure_pieces[p], 1.0/gamma_pieces[p]);
+  return density_pieces[p] *
+         std::pow(P / pressure_pieces[p], 1.0 / gamma_pieces[p]);
+}
+
+Real ColdPiecewisePolytrope::DensityFromEnergy(Real e)
+{
+  assert(IsInitialized());
+  if (T > 1e-10)
+  {
+    throw std::runtime_error(
+      "ColdPiecewisePolytrope::DensityFromEnergy only implemented for T=0");
+  }
+  // Unfortunately, e(rho) cannot be inverted simply. Therefore, we use a
+  // Newton-Raphson solver to get this instead.
+  int p   = FindPieceFromE(e);
+  Real lb = density_pieces[p];
+  Real ub = density_pieces[p + 1];
+  auto f  = [&](Real n) -> Real { return GetColdEnergy(n, p) - e; };
+  auto df = [&](Real n) -> Real
+  {
+    return mb * (1.0 + eps_pieces[p]) + gamma_pieces[p] *
+                                          GetColdPressure(n, p) /
+                                          (n * (gamma_pieces[p] - 1.0));
+  };
+  Real flb       = f(lb);
+  Real fub       = f(ub);
+  Real x         = (fub * lb - flb * ub) / (fub - flb);
+  Real fx        = f(x);
+  const Real tol = 1e-15;
+  while (abs(fx) > e * tol)
+  {
+    Real xnew = x - fx / df(x);
+    // If the guess is no good, throw it away and use bisection instead.
+    if (xnew > ub || xnew < lb)
+    {
+      xnew = 0.5 * (ub + lb);
+    }
+    fx = f(xnew);
+    if (fx > 0)
+    {
+      ub = xnew;
+    }
+    else
+    {
+      lb = xnew;
+    }
+    x = xnew;
+  }
+
+  return x;
 }
--- src/z4c/primitive/cold_piecewise_polytrope.hpp
+++ src/z4c/primitive/cold_piecewise_polytrope.hpp
@@ -10,59 +10,66 @@
 #include "coldeos_policy_interface.hpp"
 #include "piecewise_polytrope.hpp"
 
-namespace Primitive {
+namespace Primitive
+{
 
-  class ColdPiecewisePolytrope: public PiecewisePolytrope {
-
+class ColdPiecewisePolytrope : public PiecewisePolytrope
+{
   protected:
+  /// Constructor
+  ColdPiecewisePolytrope();
 
-    /// Constructor
-    ColdPiecewisePolytrope();
+  /// Destructor
+  ~ColdPiecewisePolytrope();
 
-    /// Destructor
-    ~ColdPiecewisePolytrope();
+  /// Calculate the pressure from the number density
+  Real Pressure(Real n);
 
-    /// Calculate the pressure from the number density
-    Real Pressure(Real n);
+  /// Calculate the energy from the number density
+  Real Energy(Real n);
 
-    /// Calculate the energy from the number density
-    Real Energy(Real n);
+  /// Calculate the derivative of the pressure wrt. the numberdensity from the
+  /// number density
+  Real dPdn(Real n);
 
-    /// Calculate the derivative of the pressure wrt. the numberdensity from the number density
-    Real dPdn(Real n);
+  /// Calculate the specific internal energy from the number density
+  Real SpecificInternalEnergy(Real n);
 
-    /// Calculate the specific internal energy from the number density
-    Real SpecificInternalEnergy(Real n);
+  /// Calculate the abundance of species iy from the number density
+  Real Y(Real n, int iy);
 
-    /// Calculate the abundance of species iy from the number density
-    Real Y(Real n, int iy);
+  /// Calculate the specific enthalpy from the number density
+  Real Enthalpy(Real n);
 
-    /// Calculate the specific enthalpy from the number density
-    Real Enthalpy(Real n);
+  /// Find the index of the piece that the pressure aligns with.
+  int FindPieceFromP(Real P) const;
 
+  /// Find the index of the piece that the energy aligns with.
+  int FindPieceFromE(Real e) const;
 
-    /// Find the index of the piece that the pressure aligns with.
-    int FindPieceFromP(Real P) const;
+  /// Calculate the number density from the pressure
+  Real DensityFromPressure(Real P);
 
-    /// Calculate the number density from the pressure
-    Real DensityFromPressure(Real P);
+  /// Calculate the number density from the energy density
+  Real DensityFromEnergy(Real e);
 
-    /// temperature of the slice
-    Real T;
+  /// temperature of the slice
+  Real T;
 
   public:
+  /// Set the Temperature
+  inline void SetTemperature(Real t)
+  {
+    T = t;
+  }
 
-    /// Set the Temperature
-    inline void SetTemperature(Real t) {
-      T = t;
-    }
-
-    /// Get the Temperature
-    inline Real GetTemperature() {
-      return T * eos_units->TemperatureConversion(*code_units);
-    }
+  /// Get the Temperature
+  inline Real GetTemperature()
+  {
+    return T * eos_units->TemperatureConversion(*code_units);
+  }
 };
 
-} // namespace Primitive
+}  // namespace Primitive
 
 #endif
